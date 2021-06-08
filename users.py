04bac1bf35b5d709c1d1512de6fdd27b0e1cb7b7
DaheeLee111 <mizl56789@gmail.com>
2021-06-08 23:22:38 +0900

    commit

--- users.py
+++ users.py
@@ -1,4 +1,5 @@
 # 블록을 만들어주는 클래스 
+# 만들고 보니까 걍 튜플 이나 딕셔너리 형식으로 만드는게 더 나을거같음 
 class block:
     def setdata(self, data, hash_, previoushash, time):
         self.data = data
@@ -14,14 +15,17 @@ def proof_of_work(hash,data):
 # 블록을 연결해주는 함수
 import datetime
 def chaining_block(data, hash_, previoushash):
+    # 블록 만들기
     currunt_block = block()
     currunt_block.setdata(data, hash_, previoushash, datetime.datetime.today().strftime('%Y-%m-%d-%H-%M-%S'))
-    return currunt_block
+    # 블록 연결 안썼음
 
 # 해시 만들어주는 함수
 import hashlib
 def making_hash(word):
     return hashlib.sha256(word.encode()).hexdigest()
+# word넣으면 해시 나옴 
 
 if __name__ == '__main__':
+    # 여기다 실행할거 쓰면댐
     pass
