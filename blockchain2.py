4b7c36dc412e5aadfa7a0c9ee781aeb166c29d8e
DaheeLee111 <mizl56789@gmail.com>
2021-06-09 12:18:35 +0900

    commit

--- blockchain2.py
+++ blockchain2.py
@@ -15,18 +15,20 @@ class Blockchain(object):
         pass
     def last_block(self):
         pass 
+    
 def new_block(self,proof,previous_hash=None):
     #블록생성정보나오는거
     block = {
-            'index': len(self.chain)+1,
-            'timestamp': time(),
-            'transactions': self.current_transaction,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        'index': len(self.chain)+1,
+        'timestamp': time(),
+        'transactions': self.current_transaction,
+        'proof': proof,
+        'previous_hash': previous_hash or self.hash(self.chain[-1])
     }
     self.current_transaction = []
     self.chain.append(block)
     return block
+
 def new_transaction(self,sender,recipient,amount):
     #거래정보나오는거
     self.current_transaction.append(
@@ -40,11 +42,13 @@ def new_transaction(self,sender,recipient,amount):
 def hash(block):
     block_string = json.dumps(block, sort_keys=True).encode
     return hashlib.sha256(block_string).hexdigest()
+
 def proof_of_work(self, last_proof):
     proof=0
     while self.valid_proof(last_proof,proof)is False:
         proof+=1
     return proof
+
 def valid_proof(last_proof,proof):
     guess = str(last_proof + proof).encode()
     guess_hash = hashlib.sha256(guess).hexigest()
